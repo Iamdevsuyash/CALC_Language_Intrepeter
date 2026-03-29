03d428ec7e2927c4c7d5a50fea5766dd29776720
NIRMAL20062 <kumar.nirmal2608@gmail.com>
2026-03-29 16:07:53 +0530

    Add token mappings for two-character and one-character tokens in Tokenizer class

--- tokenizer/Tokenizer.java
+++ tokenizer/Tokenizer.java
@@ -12,13 +12,26 @@ public class Tokenizer {
 
     //define the two character tokens and their corresponding types
     private static final Map<String, Token.Type> twoChar = Map.of(
-       
+        ":=", Token.Type.ASSIGN,
+        "==", Token.Type.EQ,
+        "=>", Token.Type.ARROW,
+        ">>", Token.Type.PRINT
     );
 
 
     // define the one character tokens and their corresponding types
     private static final Map<Character, Token.Type> oneChar = Map.ofEntries(
-        
+        Map.entry('+', Token.Type.PLUS),
+        Map.entry('-', Token.Type.MINUS),
+        Map.entry('*', Token.Type.MULTIPLY),
+        Map.entry('/', Token.Type.DIVIDE),
+        Map.entry('(', Token.Type.LPAREN),
+        Map.entry(')', Token.Type.RPAREN),
+        Map.entry('<', Token.Type.LT),
+        Map.entry('>', Token.Type.GT),
+        Map.entry('?', Token.Type.IF),
+        Map.entry('@', Token.Type.REPEAT),
+        Map.entry(':', Token.Type.COLON)
     );
 
 }
