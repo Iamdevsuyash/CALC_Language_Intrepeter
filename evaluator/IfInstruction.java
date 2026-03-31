ac59c13ccaa47b64bc10f057d7df6191d9c54802
IamDevsuyash <yadavsuyash53@gmail.com>
2026-03-31 22:30:44 +0530

    Make RepeatInstruction use Expression and added type safety in If block condition expression

--- evaluator/IfInstruction.java
+++ evaluator/IfInstruction.java
@@ -1,15 +1,15 @@
 package evaluator;
+
 import parser.Expression;
 import java.util.List;
 
-
-public class IfInstruction implements Instruction{
+public class IfInstruction implements Instruction {
     // Store: the condition expression and a List<Instruction> for the body.
     private final Expression condition;
     private final List<Instruction> ifbody;
     private final List<Instruction> elsebody;
 
-    IfInstruction(Expression condition,List<Instruction> body,List<Instruction> elsebody){
+    IfInstruction(Expression condition, List<Instruction> body, List<Instruction> elsebody) {
         this.condition = condition;
         this.ifbody = body;
         this.elsebody = elsebody;
@@ -18,20 +18,25 @@ public class IfInstruction implements Instruction{
     @Override
     public void execute(Environment env) {
         // Evaluate the condition.
-        boolean check = (boolean) condition.evaluate(env);
+        Object result = condition.evaluate(env);
+        if (!(result instanceof Boolean)) {
+            throw new RuntimeException("Condition must be boolean");
+        }
+
+        boolean check = (boolean) result;
 
         // If the result is true, execute each instruction in the body.
-        if (check == true){
-            for (Instruction i:ifbody){
+        if (check) {
+            for (Instruction i : ifbody) {
                 i.execute(env);
             }
-        }
-        else{
-            for (Instruction i:elsebody){
-                i.execute(env);
+        } else {
+            if (elsebody != null) {
+                for (Instruction i : elsebody) {
+                    i.execute(env);
+                }
             }
         }
-
 
     }
 
--- evaluator/RepeatInstruction.java
+++ evaluator/RepeatInstruction.java
@@ -1,19 +1,22 @@
 package evaluator;
+import parser.Expression;
 import java.util.List;
 
 public class RepeatInstruction implements Instruction {
     // Store: the repeat count and a List<Instruction> for the body.
-    private final Integer count;
+    private final Expression count;
     private final List<Instruction> body;
 
-    RepeatInstruction(Integer count,List<Instruction> b){
+    RepeatInstruction(Expression count,List<Instruction> b){
         this.count = count;
         this.body = b;
     }
     @Override
     public void execute(Environment env) {
+        int times = (int) count.evaluate(env);
+
         // Execute all body instructions, repeated count times.
-        for (int i = 0; i< this.count;i++){
+        for (int i = 0; i< times;i++){
             for (Instruction ins:this.body){
                 ins.execute(env);
             }
